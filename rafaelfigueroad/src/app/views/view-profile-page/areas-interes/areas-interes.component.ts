8e2acf42ed092f54083f0cc49e2f36936383c72f
PicosB <brayan.garcia232168@potros.itson.edu.mx>
2025-05-02 15:26:06 -0600

    Added Modals

--- rafaelfigueroad/src/app/views/view-profile-page/areas-interes/areas-interes.component.ts
+++ rafaelfigueroad/src/app/views/view-profile-page/areas-interes/areas-interes.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-areas-interes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './areas-interes.component.html',
   styleUrl: './areas-interes.component.css',
 })
 export class AreasInteresComponent {
+  modalActivo: string | null = null;
 
+  abrirModal(area: string) {
+    this.modalActivo = area;
+  }
+
+  cerrarModal() {
+    this.modalActivo = null;
+  }
 }
